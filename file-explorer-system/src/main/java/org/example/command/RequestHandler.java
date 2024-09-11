ca0df89a10daac9077000524ffc8c40eaa74c057
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:09:55 +0200

    Update RequestHandler.java

--- file-explorer-system/src/main/java/org/example/command/RequestHandler.java
+++ file-explorer-system/src/main/java/org/example/command/RequestHandler.java
@@ -3,9 +3,11 @@ package org.example.command;
 import org.example.enums.AppCommand;
 import org.example.service.FileService;
 
+import java.io.File;
+
 public abstract class RequestHandler {
 
-    public static Command getRequest(AppCommand state, FileService service, String[] fileInfo) {
+    public static Command getRequest(File root, AppCommand state, FileService service, String[] fileInfo) {
 
         if (state != AppCommand.INVALID_VALUE) {
 
@@ -13,7 +15,7 @@ public abstract class RequestHandler {
 
                 case CREATE_DIR, CREATE_FILE:
                     if (fileInfo.length == 1) {
-                        return new Add(state, service, fileInfo[0].trim());
+                        return new Add(root, state, service, fileInfo[0].trim());
                     } else {
                         System.out.println("Not a valid file name.");
                     }
@@ -21,7 +23,7 @@ public abstract class RequestHandler {
 
                 case DELETE:
                     if (fileInfo.length == 1) {
-                        return new Delete(service, fileInfo[0].trim());
+                        return new Delete(root, service, fileInfo[0].trim());
                     } else {
                         System.out.println("Not a valid file name.");
                     }
@@ -29,7 +31,7 @@ public abstract class RequestHandler {
 
                 case UPDATE:
                     if (fileInfo.length == 2) {
-                        return new Update(service, fileInfo);
+                        return new Update(root, service, fileInfo);
                     } else {
                         System.out.println("Missing input for updating file name.");
                     }
