ae9f1559a9fefdbf4e3d87135b6d5e40d417f16a
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:43:24 +0200

    Update FileCreator.java

--- file-explorer-system/src/main/java/org/example/service/FileCreator.java
+++ file-explorer-system/src/main/java/org/example/service/FileCreator.java
@@ -1,7 +1,5 @@
 package org.example.service;
 
-import org.example.enums.AppCommand;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -9,15 +7,7 @@ public final class FileCreator {
 
     private FileCreator() {}
 
-    public static boolean addNewFile(File root, AppCommand state, String fileName) throws IOException {
-        return switch (state) {
-            case CREATE_FILE -> createNewFile(root, fileName);
-            case CREATE_DIR -> createNewDirectory(root, fileName);
-            default -> false;
-        };
-    }
-
-    private static boolean createNewFile(File root, String fileName) throws IOException {
+    public static boolean createNewFile(File root, String fileName) throws IOException {
         File file = new File(root + File.separator + fileName);
         boolean fileCreated = false;
         if (!file.exists()) {
@@ -26,7 +16,7 @@ public final class FileCreator {
         return fileCreated;
     }
 
-    private static boolean createNewDirectory(File root, String directoryName) {
+    public static boolean createNewDirectory(File root, String directoryName) {
         File directory = new File(root + File.separator + directoryName);
         boolean directoryCreated = false;
         if (!directory.exists()) {
