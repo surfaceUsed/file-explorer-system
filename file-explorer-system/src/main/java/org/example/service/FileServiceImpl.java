d261743cf5d8652ec55129a3c8771b513516ad95
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:44:09 +0200

    Update FileServiceImpl.java

--- file-explorer-system/src/main/java/org/example/service/FileServiceImpl.java
+++ file-explorer-system/src/main/java/org/example/service/FileServiceImpl.java
@@ -1,21 +1,28 @@
 package org.example.service;
 
-import org.example.enums.AppCommand;
-
 import java.io.File;
 import java.io.IOException;
 
 public class FileServiceImpl implements FileService {
 
     @Override
-    public void createNewFile(File root, AppCommand state, String fileName) throws IOException {
-        if (FileCreator.addNewFile(root, state, fileName)) {
-            System.out.println("File created successfully in directory \"" + root + "\".");
+    public void createNewDirectory(File root, String directoryName) {
+        if (FileCreator.createNewDirectory(root, directoryName)) {
+            System.out.println("Directory created successfully in \"" + root + "\".");
         } else {
-            System.out.println("Failed to create new file in directory \"" + root + "\".");
+            System.out.println("Failed to create new directory in \"" + root + "\".");
         }
     }
-    
+
+    @Override
+    public void createNewFile(File root, String fileName) throws IOException {
+        if (FileCreator.createNewFile(root, fileName)) {
+            System.out.println("File created successfully in \"" + root + "\".");
+        } else {
+            System.out.println("Failed to create new file in \"" + root + "\".");
+        }
+    }
+
     @Override
     public void deleteFile(File root, String fileName) {
         if (FileRemover.deleteFile(root, fileName)) {
@@ -26,9 +33,9 @@ public class FileServiceImpl implements FileService {
     }
 
     @Override
-    public void updateFileName(File root, String[] fileInfo) {
-        File filePathToUpdate = new File(root + File.separator + fileInfo[0].trim());
-        if (FileUpdater.renameFile(filePathToUpdate, fileInfo[1].trim())) {
+    public void updateFileName(File root, String fileName, String updatedName) {
+        File filePathToUpdate = new File(root + File.separator + fileName);
+        if (FileUpdater.renameFile(filePathToUpdate, updatedName)) {
             System.out.println("File name updated.");
         } else {
             System.out.println("Failed to rename file since it doesn't exist in directory.");
