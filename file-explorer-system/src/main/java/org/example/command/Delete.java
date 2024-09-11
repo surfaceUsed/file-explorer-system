2240ff8e603822876a6d8ad39074170b3d8fc9fd
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:09:24 +0200

    Update Delete.java

--- file-explorer-system/src/main/java/org/example/command/Delete.java
+++ file-explorer-system/src/main/java/org/example/command/Delete.java
@@ -2,18 +2,22 @@ package org.example.command;
 
 import org.example.service.FileService;
 
+import java.io.File;
+
 class Delete implements Command {
 
+    private final File root;
     private final FileService fileSystem;
     private final String fileName;
 
-    Delete(FileService fileSystem, String fileName) {
+    Delete(File root, FileService fileSystem, String fileName) {
+        this.root = root;
         this.fileSystem = fileSystem;
         this.fileName = fileName;
     }
 
     @Override
     public void execute() {
-        this.fileSystem.deleteFile(this.fileName);
+        this.fileSystem.deleteFile(this.root, this.fileName);
     }
 }
