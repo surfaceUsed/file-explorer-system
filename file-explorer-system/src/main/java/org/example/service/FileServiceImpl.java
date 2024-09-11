a2018905a29389e8295ad2ed173e52287208b583
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:07:47 +0200

    Update FileServiceImpl.java

--- file-explorer-system/src/main/java/org/example/service/FileServiceImpl.java
+++ file-explorer-system/src/main/java/org/example/service/FileServiceImpl.java
@@ -7,38 +7,31 @@ import java.io.IOException;
 
 public class FileServiceImpl implements FileService {
 
-    private File root;
-
     @Override
-    public void updateCurrentDirectory(File newDirectory) {
-        this.root = newDirectory;
-    }
-
-    @Override
-    public void createNewFile(AppCommand state, String fileName) throws IOException {
-        if (FileCreator.addNewFile(this.root, state, fileName)) {
-            System.out.println("File created successfully in directory \"" + this.root + "\".");
+    public void createNewFile(File root, AppCommand state, String fileName) throws IOException {
+        if (FileCreator.addNewFile(root, state, fileName)) {
+            System.out.println("File created successfully in directory \"" + root + "\".");
         } else {
-            System.out.println("Failed to create new file in directory \"" + this.root + "\".");
+            System.out.println("Failed to create new file in directory \"" + root + "\".");
+        }
+    }
+    
+    @Override
+    public void deleteFile(File root, String fileName) {
+        if (FileRemover.deleteFile(root, fileName)) {
+            System.out.println("File(s) removed from directory \"" + root + "\".");
+        } else {
+            System.out.println("Deleting file(s) from directory \"" + root + "\" failed.");
         }
     }
 
     @Override
-    public void deleteFile(String fileName) {
-        if (FileRemover.deleteFile(this.root, fileName)) {
-            System.out.println("File(s) removed from directory \"" + this.root + "\".");
+    public void updateFileName(File root, String[] fileInfo) {
+        File filePathToUpdate = new File(root + File.separator + fileInfo[0].trim());
+        if (FileUpdater.renameFile(filePathToUpdate, fileInfo[1].trim())) {
+            System.out.println("File name updated.");
         } else {
-            System.out.println("Deleting file(s) from directory \"" + this.root + "\" failed.");
+            System.out.println("Failed to rename file since it doesn't exist in directory.");
         }
-    }
-
-   @Override
-    public void updateFileName(String[] fileInfo) {
-       File filePathToUpdate = new File(this.root + File.separator + fileInfo[0].trim());
-       if (FileUpdater.renameFile(filePathToUpdate, fileInfo[1].trim())) {
-           System.out.println("File name updated.");
-       } else {
-           System.out.println("Failed to rename file since it doesn't exist in directory.");
-       }
     }
 }
