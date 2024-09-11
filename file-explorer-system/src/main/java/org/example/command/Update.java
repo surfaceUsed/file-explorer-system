b4e7c4f3a135972de7fd8d88e2d0061c447f36e2
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-09-11 12:10:16 +0200

    Update Update.java

--- file-explorer-system/src/main/java/org/example/command/Update.java
+++ file-explorer-system/src/main/java/org/example/command/Update.java
@@ -2,18 +2,22 @@ package org.example.command;
 
 import org.example.service.FileService;
 
+import java.io.File;
+
 class Update implements Command {
 
+    private final File root;
     private final FileService fileSystem;
     private final String[] fileInfo;
 
-    Update(FileService fileSystem, String[] fileInfo) {
+    Update(File root, FileService fileSystem, String[] fileInfo) {
+        this.root = root;
         this.fileSystem = fileSystem;
         this.fileInfo = fileInfo;
     }
 
     @Override
     public void execute() {
-        this.fileSystem.updateFileName(this.fileInfo);
+        this.fileSystem.updateFileName(this.root, this.fileInfo);
     }
 }
